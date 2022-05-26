ae69040522f2fd51e3ac805140283dc59cbf2ef6
Trigha <adhindyatrigha@gmail.com>
2022-05-26 22:43:59 +0700

    add navbar

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+    theme: {
+      extend: {
+        colors: {
+         blossom: '#EEBECE',
+         mainBlue: '#0B24FB',
+        },
+        layout:{
+          'blueTop': {left:-377, top: 0, width: '100%', height: '100%'},
+        },
+        fontFamily: {
+          mulish: ["Mulish", "sans-serif"],
+        },
+        screens: {
+          'laptop': '1920px',
+        },
+        container: {
+          center: true,
+        },
+      },
+    },
+    plugins: [],
+  }
